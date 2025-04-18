85fdf1129f5b8ec184f1a2ac836b2202a156d171
Quantumdrill <quantumdrill317@gmail.com>
2025-04-17 23:02:20 -0400

    p

--- server.js
+++ server.js
@@ -1,14 +1,22 @@
 let express = require('express');
+const https = require('https');
+let fs = require("fs")
 
 let app = express();
 
 app.use(express.static('static'));
 app.use('/shichen', require('./shichen/server_shichen'));
 
+const options = {
+  key: fs.readFileSync('/etc/letsencrypt/live/tobylu-qd.com/privkey.pem'),
+  cert: fs.readFileSync('/etc/letsencrypt/live/tobylu-qd.com/fullchain.pem')
+};
+
+
 app.get("/", (req,res)=>{
-  res.redirect("/shichen")
+
 })
 
-app.listen(80, function () {
+https.createServer(options, app).listen(443, ()=>{
   console.log("http://127.0.0.1:80")
 });
